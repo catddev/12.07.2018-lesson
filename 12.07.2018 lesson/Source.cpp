77c0467b438d6e5edabf5cd40da6671cddf19abd
айкеновад <@234_12>
2018-07-12 12:16:58 +0600

    -

--- 12.07.2018 lesson/Source.cpp
+++ 12.07.2018 lesson/Source.cpp
@@ -153,6 +153,12 @@ start:
 	break;
 	case 5:
 	{
+
+		// В двухмерном массиве целых чисел посчитать сумму элементов:
+		// a)	в каждой строке
+		// b)	в каждом столбце
+		// c)	*** одновременно по всем строкам и всем столбцам.
+
 		int a[3][4];
 		int sum = 0;
 		int sum1 = 0;
@@ -184,27 +190,110 @@ start:
 			}
 			cout << left << setw(3) << sum2 << " ";
 		}
-
 		cout << " " << sum << endl << endl;
-
-
-
-
 	}
 	break;
 	case 6:
 	{
+		// Создайте одномерный массив. Заполните его случайными числами и покажите на экран.
+		// Пользователь выбирает количество сдвигов и положение (влево, вправо).
+		// Выполнить сдвиг массива и показать на экран полученный результат. Сдвиг циклический. 
+		// Например, если мы имеем следующий массив
+		//	1 2 0 4 6 7 9
+		//	и пользователь выбрал сдвиг на 2 разряда влево, то мы получим
+		//	0 4 6 7 9 1 2
+
+		const int n = 7;
+		int a[n];
+		int b[n];
+		int i = 0, j = 0;
+		int move;
+		int direction;
+
+		for (i = 0; i < n; i++)
+		{
+			a[i] = rand() % 10;
+			cout << a[i] << " ";
+		}
+		cout << endl << endl;
+
+		cout << "выберите количество сдвигов: ";
+		cin >> move;
+	/*	cout << "выберите положение (1 - влево, 2 - вправо): ";
+		cin >> direction;
+
+		if (direction == 1)
+		{*/
+			for (i = 0, j=abs(n-move-i); i < n; i++, j++)
+			{
+				b[move + i] = a[i];
+
+				b[j] = a[i+move];
+			}
+
+			
+			for (i = 0; i < n; i++)
+				cout << a[i] << " ";
+		
+			cout << endl << endl;
+		/*}
+		else
+		{
+
+		}*/
 
 	}
 	break;
-	case 7:
+	case 7: // the same as previous
 	{
+		int k;
+		cin >> k;
+		const int n = 6;
+
+		int a[n] = { 1, 2, 3, 4, 5, 6 };
+		int b[n];
+
+		for (int i = 0; i < n; i++)
+		{
+			if (i + k < n)
+				b[i + k] = a[i];
+			else
+				b[abs(n - k - i)] = a[i];
+		}
+
+		for (int i = 0; i < n; i++)
+			cout << b[i] << " ";
+
+		cout << endl << endl;
 
 	}
 	break;
 	case 8:
 	{
+		const int n = 10;
+		int a[n][n];
 
+		int i, j, p, k;
+		cin >> k;
+		cout << endl;
+
+		for (int p = 0; p < k; p++)
+		{
+			for (i = 0; i < k; i++)
+			{
+				for (j = 0; j < k; j++)
+				{
+					a[i][j] = i + 1;
+					cout << a[i][j] << " ";
+				}
+
+				cout << endl;
+			}
+			cout << endl;
+		}
+		
+
+		
 	}
 	break;
 	case 9:
