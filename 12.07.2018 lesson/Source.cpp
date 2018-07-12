b1f788b201ff8f3d7a1e7e074f31b829c2705c89
айкеновад <@234_12>
2018-07-12 09:05:48 +0600

    -

--- 12.07.2018 lesson/Source.cpp
+++ 12.07.2018 lesson/Source.cpp
@@ -26,68 +26,7 @@ start:
 	{
 	case 1:
 	{
-		const int n = 20;
-		int a[n];
-		int size;
-
-		double sum = 0;
-		int i, pass;
-		int border;
-
-		cout << "введите количество элементов массива: ";
-		cin >> size;
-		for (i = 0; i < size; i++)
-		{
-			a[i] = -20 + rand() % 50;
-			cout << a[i] << " ";
-		}
-		cout << endl;
-
-		for (i = 0; i < n; i++)
-			sum = sum + a[i];
-		cout << "average = " << sum / n << endl;
-
-		if (sum / n > 0)
-		{
-			border = n - n / 3;
-			cout << "border = " << border << endl;
-			for (pass = 1; pass < border; pass++)
-			{
-				i = pass;
-				while ((a[i] < a[i - 1]) && i > 0)
-				{
-					swap(a[i], a[i - 1]);
-					i--;
-				}
-			}
-		}
-		else
-		{
-			border = n / 3;
-			for (pass = 1; pass < border; pass++)
-			{
-				i = pass;
-				while ((a[i] < a[i - 1]) && i > 0)
-				{
-					swap(a[i], a[i - 1]);
-					i--;
-				}
-			}
-		}
-
-		int center = n - (n - border) / 2;
-		int k;
-		for (i = border, k = n - 1; i < center; i++, k--)
-			swap(a[i], a[k]);
-
-		cout << "sorted part: ";
-		for (i = 0; i < border; i++)
-			cout << a[i] << " ";
-		cout << "reverse order part: ";
-		for (i = border; i<n; i++)
-			cout << a[i] << " ";
-
-		cout << endl << endl;
+		
 	}
 	break;
 	case 2:
